d5ed59788ae1b7dff399d8bd71a21c692b8e9353
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-10-01 14:15:14 -0400

    DP-Spring HTML/Thymeleaf: Initial commit. Project set up.

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	String firstName;
+	String lastName;
+	Grade grade;
+
+	public DemoApplication() {}
+
+	public String getFirstName() { return firstName; }
+
+	public void setFirstName(String firstName) { this.firstName = firstName; }
+
+	public String getLastName() { return lastName; }
+
+	public void setLastName(String lastName) { this.lastName = lastName; }
+
+	public Grade getGrade() { return grade; }
+
+	public void setGrade(Grade grade) { this.grade = grade; }
+}
